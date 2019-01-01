9244026ed8c98a2e1548bdad7a4db068fbdd8b27
Jaya <jayakrishna.691@gmail.com>
2019-01-01 15:50:03 -0600

    Update createupdatestack.py

--- CloudFormation/createupdatestack.py
+++ CloudFormation/createupdatestack.py
@@ -26,7 +26,7 @@ def mystack_create(stack_name, bucket_name):
     response = cf_client.create_stack(
         StackName=stack_name,
         TemplateURL='https://s3.amazonaws.com/' + bucket_name + '/' + stack_name + '.yml',
-        OnFailure='ROLLBACK'
+        OnFailure='DELETE'
     )
     print (response)
 
